29a53dc715e41bac8afc06e71fa861bdc9752d9e
AndreFernandesDev <andre.fernandesdev@gmail.com>
2023-08-04 10:30:12 +0100

    first release with template

--- templates.js
+++ templates.js
@@ -52,7 +52,7 @@ const notice = `
                       {{page.greeting}}
                     </h1>
 
-                    <p class="m-0 uppercase">
+                    <p class="m-0 mt-2">
                       {{page.greetingText}}
                     </p>
                   </td>
